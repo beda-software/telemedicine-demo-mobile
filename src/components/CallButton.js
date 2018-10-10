f8b0d5d5e2b6cca789d3498b4167708c042feea8
ruscoder <rusmiligamer@gmail.com>
2018-10-10 13:44:39 +0700

    Clean up

--- src/components/CallButton.js
+++ src/components/CallButton.js
@@ -1,60 +1,55 @@
-/*
- * Copyright (c) 2011-2018, Zingaya, Inc. All rights reserved.
- */
-
-'use strict';
-
 import React, { Component } from 'react';
 import {
-  StyleSheet,
-  View,
-  TouchableOpacity
+    StyleSheet,
+    View,
+    TouchableOpacity,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default class CallButton extends Component {
 
-  handleButtonPressed() {
-    this.props.buttonPressed();
-  }
+    handleButtonPressed() {
+        this.props.buttonPressed();
+    }
 
-  render() {
-    return (
-      <TouchableOpacity onPress={ () => this.handleButtonPressed() }>
-        <View style={ [styles.icon, { borderColor: this.props.color }] }>
-          <Icon 
-              name={ this.props.icon_name } 
-              color={ this.props.color }
-              size={ 20 } 
-              backgroundColor='transparent' />
-        </View>
-      </TouchableOpacity>
-    );
-  }
+    render() {
+        return (
+            <TouchableOpacity onPress={() => this.handleButtonPressed()}>
+                <View style={[styles.icon, { borderColor: this.props.color }]}>
+                    <Icon
+                        name={this.props.icon_name}
+                        color={this.props.color}
+                        size={20}
+                        backgroundColor='transparent'
+                    />
+                </View>
+            </TouchableOpacity>
+        );
+    }
 }
 
-var styles = StyleSheet.create({
-  icon: {
-    width: 30,
-    height: 30,
-    borderWidth: 2,
-    borderRadius: 15,
-    margin: 10,
-    alignItems: 'center', 
-    justifyContent: 'center',
-    backgroundColor:'transparent',
-  },
-  phone_icon: {
-    borderColor: '#0C90E7', 
-    marginHorizontal: 10
-  },
-  align_left: {
-    alignSelf: 'flex-start'
-  },
-  align_right: {
-    alignSelf: 'flex-end'
-  },
-  align_center: {
-    alignSelf: 'center'
-  },
+const styles = StyleSheet.create({
+    icon: {
+        width: 30,
+        height: 30,
+        borderWidth: 2,
+        borderRadius: 15,
+        margin: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'transparent',
+    },
+    phone_icon: {
+        borderColor: '#0C90E7',
+        marginHorizontal: 10,
+    },
+    align_left: {
+        alignSelf: 'flex-start',
+    },
+    align_right: {
+        alignSelf: 'flex-end',
+    },
+    align_center: {
+        alignSelf: 'center',
+    },
 });
